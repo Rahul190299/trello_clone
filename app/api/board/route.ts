909f88a21b5b04ec2e75478cef6ab27dadf3758b
Rahul190299 <rahulgheware123@gmail.com>
2025-10-27 22:45:24 +0530

    board api changes

--- app/api/board/route.ts
+++ app/api/board/route.ts
@@ -1,15 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
-
-
-export async function GET(req : NextRequest) {
-    try{
-        const { searchParams } = new URL(req.url);
+import {
+  boardService,
+} from "../../../lib/services";
+export async function GET(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const type = searchParams.get("type");
+    switch (type) {
+      case "boards":
         const userId = searchParams.get("userid");
-        if(!userId){
-            return new NextResponse('Missing user id parameter',{status:400});
+        if (!userId) {
+          return new NextResponse("Missing user id parameter", { status: 400 });
         }
+        const data = await boardService.getBoards(userId);
         
-    }catch(err){
-        return new NextResponse('Internal Error', { status: 500 });
+        break;
+      case "board":
+        break;
     }
+  } catch (err) {
+    return new NextResponse("Internal Error", { status: 500 });
+  }
 }
