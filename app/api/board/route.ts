a51ca1895cb65072ecf6d8ae892cf99a630822a5
Rahul190299 <rahulgheware123@gmail.com>
2025-10-28 22:24:44 +0530

    get boards api done

--- app/api/board/route.ts
+++ app/api/board/route.ts
@@ -1,24 +1,37 @@
 import { NextRequest, NextResponse } from "next/server";
-import {
-  boardService,
-} from "../../../lib/services";
+import { currentProfile } from "@/lib/current-profile";
+import { boardService } from "../../../lib/services";
+import { ca } from "zod/v4/locales";
 export async function GET(req: NextRequest) {
   try {
     const { searchParams } = new URL(req.url);
     const type = searchParams.get("type");
+    const profile = await currentProfile();
+    if (!profile) {
+      return new NextResponse("Missing profile cookie", { status: 400 });
+    }
     switch (type) {
       case "boards":
-        const userId = searchParams.get("userid");
-        if (!userId) {
-          return new NextResponse("Missing user id parameter", { status: 400 });
-        }
-        const data = await boardService.getBoards(userId);
-        
-        break;
+        const boards = await boardService.getBoards(profile.id);
+        return NextResponse.json(boards, { status: 200 });
       case "board":
-        break;
+        const boardId = searchParams.get('boardid');
+        if(!boardId){
+          return new NextResponse("Missing board id", { status: 400 });
+        }
+        const board = await boardService.getBoard(boardId);
+        return NextResponse.json(board, { status: 200 });
     }
   } catch (err) {
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function POST(request : NextRequest) {
+  try{
+    const body = await request.json(); // Parse the incoming JSON body
+    await boardService.createBoard(body);
+  }catch(err){
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
